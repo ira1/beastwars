e242b2076a6f7bcadf2ff6073118a7dd31ee8f6c
Carly Marzagalli Schraven <carbonhydrogenoxygen@hotmail.com>
2013-01-26 15:38:24 -0500

    15:37, Saturday
    Finished Animal movement, selection and seperate species to be added.

--- Beast Wars/Assets/Scripts/AnimalMovement.cs
+++ Beast Wars/Assets/Scripts/AnimalMovement.cs
@@ -2,23 +2,42 @@ using UnityEngine;
 using System.Collections;
 
 public class AnimalMovement : MonoBehaviour {
+	public Texture jumping;
+	public Texture ground;
+	public float timetoswap;
+	public float switchtime=1;
 	public Vector3 destination=new Vector3();
 	public float speed=100;
 	// Use this for initialization
 	void Start () {
 	destination=transform.position;
 	}
+
+	void SwapTextures ()
+	{
+	if(timetoswap<Time.time){
+	if(this.renderer.material.mainTexture==jumping)
+		this.renderer.material.mainTexture=ground;
+	else
+		this.renderer.material.mainTexture=jumping;
+	timetoswap=Time.time+switchtime;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
 	if(Input.GetMouseButtonDown(0))
 		onclick();
-	Vector3 direction=destination-transform.position;
+	Vector3 to_destination=destination-transform.position;
+		to_destination.y=0;
+		Vector3 direction=to_destination;
 		direction.Normalize();
 	Vector3 velocity=direction*speed;
 		
 		rigidbody.AddForce(-rigidbody.velocity, ForceMode.VelocityChange);
-		rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+		if(to_destination.magnitude>Time.deltaTime*speed)
+			rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+		SwapTextures();
 	}
 public void onclick(){
 	Camera camera=Camera.mainCamera;
